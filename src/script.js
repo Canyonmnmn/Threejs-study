8836a036f1e04cce59165164501e46797fea50a4
canyon <1225426443@qq.com>
2023-10-31 09:01:14 +0800

    feat

--- src/script.js
+++ src/script.js
@@ -1,9 +1,7 @@
 import * as THREE from "three";
 import * as lil from "lil-gui";
-import "./style.css";
-import gradientImg from "../static/textures/gradient/3.jpg";
-import { gsap } from "gsap";
-
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import CANNON from "cannon";
 /**
  * 基础
  */
@@ -18,76 +16,46 @@ const sizes = {
  * 调试器
  */
 const gui = new lil.GUI();
-const parameters = {};
-parameters.color = "#ffeded";
 
-gui.addColor(parameters, "color").onChange(() => {
-  material.color.set(parameters.color);
-});
 /**
  * 纹理
  */
 const textureLoader = new THREE.TextureLoader();
-const gradientTexture = textureLoader.load(gradientImg);
-gradientTexture.magFilter = THREE.NearestFilter;
+/**
+ * cannon  物理世界
+ */
+const world = new CANNON.World();
 /**
  * 对象
  */
-const objectDistance = 6;
-
-const material = new THREE.MeshToonMaterial({
-  color: parameters.color,
-  gradientMap: gradientTexture,
-});
-
-const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material);
-const mesh2 = new THREE.Mesh(new THREE.ConeGeometry(1, 2, 32), material);
-const mesh3 = new THREE.Mesh(
-  new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
-  material
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 32, 32),
+  new THREE.MeshStandardMaterial({})
 );
-mesh1.position.y = 0;
-mesh2.position.y = -objectDistance * 1;
-mesh3.position.y = -objectDistance * 2;
+sphere.castShadow = true;
+sphere.position.y = 0.5;
+scene.add(sphere);
 
-mesh1.position.x = 3;
-mesh2.position.x = -3;
-mesh3.position.x = 3;
-
-scene.add(mesh1, mesh2, mesh3);
-const meshArray = [mesh1, mesh2, mesh3];
-//粒子
-const count = 200;
-const particlesGeometry = new THREE.BufferGeometry();
-const particlesMaterial = new THREE.PointsMaterial({
-  sizeAttenuation: true,
-  size: 0.03,
-  color: parameters.color,
-});
-const particles = new THREE.Points(particlesGeometry, particlesMaterial);
-
-const positions = new Float32Array(count * 3);
-for (let i = 0; i < count; i++) {
-  const i3 = i * 3;
-  positions[i3 + 0] = (Math.random() - 0.5) * 10;
-  positions[i3 + 1] = (Math.random() - 0.5) * 30;
-  positions[i3 + 2] = Math.random() * 3;
-}
-particlesGeometry.setAttribute(
-  "position",
-  new THREE.BufferAttribute(positions, 3)
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(10, 10),
+  new THREE.MeshStandardMaterial({
+    color: "#777777",
+    metalness: 0.3,
+    roughness: 0.4,
+  })
 );
-scene.add(particles);
+floor.receiveShadow = true;
 
+floor.rotation.x = -Math.PI * 0.5;
+scene.add(floor);
 /**
  * 灯光
  */
 const light = new THREE.DirectionalLight("#ffffff", 1);
-light.position.set(1, 1, 0);
+light.castShadow = true;
+light.position.set(2, 2, 0);
 scene.add(light);
 
-const directionalLightHelper = new THREE.DirectionalLightHelper(light, 0.2);
-scene.add(directionalLightHelper);
 /**
  * 鼠标事件
  */
@@ -122,47 +90,24 @@ window.addEventListener("dblclick", () => {
 /**
  * 相机
  */
-const cameraGroup = new THREE.Group();
-
 const camera = new THREE.PerspectiveCamera(
   75,
   sizes.width / sizes.height,
   0.1,
   100
 );
-camera.position.set(0, 0, 5);
-cameraGroup.add(camera);
-scene.add(cameraGroup);
-/**
- * 滚动
- */
-let scrollY = window.scrollY;
-let currentPage = 0;
-window.addEventListener("scroll", () => {
-  scrollY = window.scrollY;
-  const page = Math.round(scrollY / sizes.height);
-  if (page != currentPage) {
-    currentPage = page;
-    gsap.to(meshArray[currentPage].rotation, {
-      duration: 1.5,
-      ease: "power2.inOut",
-      x: "+=6",
-      y: "+=3",
-      z: "+=1.5",
-    });
-  }
-});
+camera.position.set(-3, 3, 3);
+scene.add(camera);
 /**
  * 渲染器
  */
 
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
-  alpha: true,
 });
 
 renderer.setSize(sizes.width, sizes.height);
-
+renderer.shadowMap.enabled = true;
 /**
  * 更新屏幕变化
  */
@@ -179,22 +124,10 @@ window.addEventListener("resize", () => {
 /**
  * 内置控制
  */
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 const clock = new THREE.Clock();
-let previousTime = 0;
 const tick = () => {
-  const time = clock.getElapsedTime();
-  const deltaTime = time - previousTime;
-  previousTime = time;
-  camera.position.y = (-scrollY / sizes.height) * objectDistance;
-
-  cameraGroup.position.x += (cursor.x - cameraGroup.position.x) * 2 * deltaTime;
-  cameraGroup.position.y +=
-    (-cursor.y - cameraGroup.position.y) * 2 * deltaTime;
-
-  for (const item of meshArray) {
-    item.rotation.x += deltaTime * 0.1;
-    item.rotation.y += deltaTime * 0.2;
-  }
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
 };
