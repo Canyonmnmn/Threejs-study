c50738fa18f091a988f4e6320eeded701c5a8682
canyon <1225426443@qq.com>
2023-11-20 17:56:51 +0800

    init

--- src/script.js
+++ src/script.js
@@ -1,259 +1,118 @@
-import * as THREE from "three";
-import * as lil from "lil-gui";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import CANNON, { Vec3 } from "cannon";
-import sounds from "../static/sounds/hit.mp3"
-/**
- * 基础
- */
-const canvas = document.querySelector("#webgl");
-const scene = new THREE.Scene();
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import GUI from 'lil-gui'
 
 /**
- * 调试器
+ * Base
  */
-const gui = new lil.GUI();
-const debugObject = {};
-debugObject.createSphere = () => {
-  createSphere(Math.random() * 0.5, {
-    x: (Math.random() - 0.5) * 3,
-    y: 3,
-    z: (Math.random() - 0.5) * 3,
-  });
-};
-debugObject.createBox = () => {
-  createBox(Math.random() * 0.5, Math.random() * 0.5, Math.random() * 0.5, {
-    x: (Math.random() - 0.5) * 3,
-    y: 3,
-    z: (Math.random() - 0.5) * 3,
-  });
-};
-gui.add(debugObject, "createSphere");
-gui.add(debugObject, "createBox");
+// Debug
+const gui = new GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
 
 /**
- * 纹理
+ * Floor
  */
-const textureLoader = new THREE.TextureLoader();
-/**
- * 声音
- */
-const hitSound = new Audio(sounds)
-const playSound = (collision) =>{
-  const impactStrength = collision.contact.getImpactVelocityAlongNormal()
-  if(impactStrength > 1.5){
-    hitSound.volume = Math.random()
-    hitSound.currentTime = 0
-    hitSound.play()
-  }
-}
-/**
- * cannon  物理世界
- */
-const world = new CANNON.World();
-// 设置物体碰撞检测模式
-world.broadphase = new CANNON.SAPBroadphase(world)
-// 静止的物体设置静止属性
-world.allowSleep = true 
-//设置重力
-world.gravity.set(0, -9.82, 0);
-
-//地板
-const floorShape = new CANNON.Plane();
-const floorBody = new CANNON.Body({
-  mass: 0,
-  shape: floorShape,
-});
-floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5);
-world.addBody(floorBody);
-//材料
-const defalutMaterial = new CANNON.Material();
-const defalutContactMaterial = new CANNON.ContactMaterial(
-  defalutMaterial,
-  defalutMaterial,
-  {
-    friction: 0.1,
-    restitution: 0.6,
-  }
-);
-world.addContactMaterial(defalutContactMaterial);
-world.defaultContactMaterial = defalutContactMaterial;
-/**
- * 对象
- */
-const objectToUpdate = [];
-const sphereGeometry = new THREE.SphereGeometry(1, 20, 20);
-const sphereMaterial = new THREE.MeshStandardMaterial({
-  metalness: 0.3,
-  roughness: 0.4,
-});
-//sphere
-const createSphere = (radius, position) => {
-  // threejs
-  const mesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
-  mesh.scale.set(radius, radius, radius);
-  mesh.castShadow = true;
-  mesh.position.copy(position);
-  scene.add(mesh);
-  //cannon
-  const shape = new CANNON.Sphere(radius);
-  const body = new CANNON.Body({
-    mass: 1,
-    position: new CANNON.Vec3(0, 3, 0),
-    shape: shape,
-    material: defalutMaterial,
-  });
-  body.position.copy(position);
-  body.addEventListener("collide",playSound)
-  world.addBody(body);
-
-  objectToUpdate.push({
-    mesh: mesh,
-    body: body,
-  });
-};
-//box
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-const boxMaterial = new THREE.MeshStandardMaterial({
-  metalness: 0.3,
-  roughness: 0.4,
-});
-const createBox = (width, height, depth, position) => {
-  // threejs
-  const mesh = new THREE.Mesh(boxGeometry, boxMaterial);
-  mesh.scale.set(width, height, depth);
-  mesh.castShadow = true;
-  mesh.position.copy(position);
-  scene.add(mesh);
-
-  //cannon
-  const shape = new CANNON.Box(
-    new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5)
-  );
-  const body = new CANNON.Body({
-    mass: 1,
-    position: new CANNON.Vec3(0, 3, 0),
-    shape: shape,
-    material: defalutMaterial,
-  });
-  body.position.copy(position);
-  body.addEventListener("collide",playSound)
-  world.addBody(body);
-
-  objectToUpdate.push({
-    mesh: mesh,
-    body: body,
-  });
-};
 const floor = new THREE.Mesh(
-  new THREE.PlaneGeometry(10, 10),
-  new THREE.MeshStandardMaterial({
-    color: "#777777",
-    metalness: 0.3,
-    roughness: 0.4,
-  })
-);
-floor.receiveShadow = true;
-
-floor.rotation.x = -Math.PI * 0.5;
-scene.add(floor);
-/**
- * 灯光
- */
-const light = new THREE.DirectionalLight("#ffffff", 1);
-light.castShadow = true;
-light.position.set(2, 2, 0);
-scene.add(light);
+    new THREE.PlaneGeometry(10, 10),
+    new THREE.MeshStandardMaterial({
+        color: '#444444',
+        metalness: 0,
+        roughness: 0.5
+    })
+)
+floor.receiveShadow = true
+floor.rotation.x = - Math.PI * 0.5
+scene.add(floor)
 
 /**
- * 鼠标事件
+ * Lights
  */
-const cursor = {
-  x: 0,
-  y: 0,
-};
-window.addEventListener("mousemove", (event) => {
-  cursor.x = event.clientX / sizes.width - 0.5;
-  cursor.y = event.clientY / sizes.height - 0.5;
-});
-//双击全屏
-// window.addEventListener("dblclick", () => {
-//   const fullscreenElement =
-//     document.fullscreenElement || document.webkitFullscreenElement;
+const ambientLight = new THREE.AmbientLight(0xffffff, 2.4)
+scene.add(ambientLight)
 
-//   if (!fullscreenElement) {
-//     if (canvas.requestFullscreen) {
-//       canvas.requestFullscreen();
-//     } else if (canvas.webkitRequestFullscreen) {
-//       canvas.webkitRequestFullscreen();
-//     }
-//   } else {
-//     if (document.exitFullscreen) {
-//       document.exitFullscreen();
-//     } else if (document.webkitExitFullscreen) {
-//       document.webkitExitFullscreen();
-//     }
-//   }
-// });
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1.8)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = - 7
+directionalLight.position.set(5, 5, 5)
+scene.add(directionalLight)
 
 /**
- * 相机
+ * Sizes
  */
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100
-);
-camera.position.set(-3, 3, 3);
-scene.add(camera);
-/**
- * 渲染器
- */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
 
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(2, 2, 2)
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.target.set(0, 0.75, 0)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-});
+    canvas: canvas
+})
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-renderer.setSize(sizes.width, sizes.height);
-renderer.shadowMap.enabled = true;
 /**
- * 更新屏幕变化
+ * Animate
  */
-window.addEventListener("resize", () => {
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
+const clock = new THREE.Clock()
+let previousTime = 0
 
-  renderer.setSize(sizes.width, sizes.height);
-  //处理像素比
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
-/**
- * 内置控制
- */
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime
+    previousTime = elapsedTime
 
-const clock = new THREE.Clock();
-let oldTime = 0;
-const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-  const deltaTime = elapsedTime - oldTime;
-  oldTime = deltaTime;
+    // Update controls
+    controls.update()
 
-  world.step(1 / 60, oldTime, 3);
+    // Render
+    renderer.render(scene, camera)
 
-  for (const object of objectToUpdate) {
-    object.mesh.position.copy(object.body.position);
-    object.mesh.quaternion.copy(object.body.quaternion);
-  }
-  renderer.render(scene, camera);
-  window.requestAnimationFrame(tick);
-};
-tick();
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
